6ae5904d3396eb0f1b3fc47cd22fea42b98a702f
rishabh <rishabh@infracloud.io>
2024-04-15 14:40:17 +0530

    Add docker install support for custom cluster

--- extensions/provisioning/customcluster.go
+++ extensions/provisioning/customcluster.go
@@ -1,8 +1,18 @@
 package provisioning
 
 import (
+	"github.com/rancher/shepherd/pkg/config"
+	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 )
+
+const (
+	installDockerConfigFileKey = "installDocker"
+)
+
+type InstallDockerConfig struct {
+	InstallDockerURL string `json:"installDockerURL" yaml:"installDockerURL"`
+}
 
 type CustomClusterConfig struct {
 	ExternalNodeProvider ExternalNodeProvider `json:"externalNodeProvider" yaml:"externalNodeProvider"`
@@ -11,4 +21,11 @@ type CustomClusterConfig struct {
 	SpecifyPrivateIP     bool                 `json:"specifyPrivateIP" yaml:"specifyPrivateIP"`
 	SpecifyPublicIP      bool                 `json:"specifyPublicIP" yaml:"specifyPublicIP"`
 	NodeNamePrefix       string               `json:"nodeNamePrefix" yaml:"nodeNamePrefix"`
+}
+
+func GetDockerInstallURL() string {
+	installDockerCfg := &InstallDockerConfig{}
+	config.LoadConfig(installDockerConfigFileKey, installDockerCfg)
+	logrus.Info(installDockerCfg.InstallDockerURL)
+	return installDockerCfg.InstallDockerURL
 }
